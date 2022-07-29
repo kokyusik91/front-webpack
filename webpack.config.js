68c80f7fad0c7e216fd120d8ba45ff06fe1af6ea
kks91 <kks91@dgdr.co.kr>
2022-07-29 09:53:38 +0900

    🔨 Add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,65 +10,65 @@
 // webpack 해석하기
 
 
-var path = require('path')
-var webpack = require('webpack')
+// var path = require('path')
+// var webpack = require('webpack')
 
-module.exports = {
-  mode: 'production',
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
+// module.exports = {
+//   mode: 'production',
+//   entry: './src/main.js',
+//   output: {
+//     path: path.resolve(__dirname, './dist'),
+//     publicPath: '/dist/',
+//     filename: 'build.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'vue-style-loader',
+//           'css-loader'
+//         ],
+//       },      
+//       {
+//         test: /\.vue$/,
+//         loader: 'vue-loader',
+//         options: {
+//           loaders: {
+//           }
+//           // other vue-loader options go here
+//         }
+//       },
+//       {
+//         test: /\.js$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/
+//       },
+//       {
+//         test: /\.(png|jpg|gif|svg)$/,
+//         loader: 'file-loader',
+//         options: {
+//           name: '[name].[ext]?[hash]'
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     alias: {
+//       'vue$': 'vue/dist/vue.esm.js'
+//     },
+//     extensions: ['*', '.js', '.vue', '.json']
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     noInfo: true,
+//     overlay: true
+//   },
+//   performance: {
+//     hints: false
+//   },
+//   devtool: '#eval-source-map'
+// }
 
 // if (process.env.NODE_ENV === 'production') {
 //   module.exports.devtool = '#source-map'
@@ -93,26 +93,26 @@ module.exports = {
 
 // webpack devserver
 
-// var path = require('path');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// module.exports = {
-//   mode: 'none',
-//   entry: './src/devServer.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'bundling'),
-//   },
-//   devServer: {
-//     port: 9000,
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
-//       template: 'webpack-devserver.html'
-//     }),
-//   ],
-// };
+module.exports = {
+  mode: 'none',
+  entry: './src/devServer.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'bundling'),
+  },
+  devServer: {
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      template: 'webpack-devserver.html'
+    }),
+  ],
+};
 
 
 
